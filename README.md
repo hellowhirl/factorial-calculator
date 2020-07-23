0e0a5818fe1a5ca4d3f6db8ca7451c9d5e896566
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-07-23 22:41:03 +0900

    implement working prototype

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Decimals will be rounded to the nearest integer
 
 Example:
 
-Input: 6
-
-[ Calculate ]
+Input: 6 [ Calculate ]
 
 Formula:
 6! = 1 x 2 x 3 x 4 x 5 x 6
 
 Result:
 720
+
+What is a factoriaL?
+-explanation
--- src/index.js
+++ src/index.js
@@ -9,13 +9,45 @@ function component() {
   return element;
 }
 
-function testing() {
-  const headerElement = document.getElementsByTagName("h1")[0];
-  headerElement.style.color = "red";
+document.body.appendChild(component());
+
+const node = document.getElementById("numberInput");
+node.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    handleClick();
+  }
+});
+
+function calculateFactorial(n) {
+  // If the number is less than 0, reject it.
+  if (n < 0) {
+    return 'input must be an integer greater than or equal to "0"';
+  }
+  // If the number is 0, its factorial is 1.
+  else if (n === 0) {
+    return 1;
+  }
+  // Otherwise, call this recursive procedure again.
+  else {
+    return n * calculateFactorial(n - 1);
+  }
 }
 
-testing();
+function handleClick() {
+  const num = document.getElementById("numberInput").value;
+  const integer = Math.round(num);
+  const result = calculateFactorial(integer);
+  document.getElementById("placeholder").innerHTML = result;
+
+  let factoring = "";
+  for (let i = 1; i <= integer; i++) {
+    factoring += i !== integer ? i + " x " : i;
+  }
+
+  document.getElementById("userInput").innerHTML = `${integer}!`;
+  document.getElementById("mathematics").innerHTML = factoring;
+}
+
+document.getElementById("calculate").addEventListener("click", handleClick);
 
 console.log("testing");
-
-document.body.appendChild(component());
