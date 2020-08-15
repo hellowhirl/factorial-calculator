19fc9a1e98c752dea063921794bfcdaedcc88736
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-08-15 21:36:45 +0900

    bundle into docs folder for GH pages

--- src/utils/handleClear.js
+++ src/utils/handleClear.js
@@ -1,5 +1,4 @@
 export const handleClear = () => {
-  console.log("works");
   document.getElementById("numberInput").value = null;
   document.getElementById("userInput").innerHTML = null;
   document.getElementById("mathematics").innerHTML = null;
