bf433303d888e47117c406b844748388beea67b7
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-09-04 09:04:24 +0900

    add validation for max input

--- src/utils/handleClick.js
+++ src/utils/handleClick.js
@@ -1,23 +1,25 @@
 import { calculateFactorial } from "./calculateFactorial";
 
-const node = document.getElementById("numberInput");
-node.addEventListener("keyup", function (event) {
+const numberInput = document.getElementById("numberInput");
+numberInput.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     handleClick();
   }
 });
 
 export const handleClick = () => {
-  const num = document.getElementById("numberInput").value;
-  const integer = Math.round(num);
-  const result = calculateFactorial(integer);
-  document.getElementById("placeholder").innerHTML = result;
+  const integer = Math.round(numberInput.value);
+  if (integer <= 200) {
+    const result = calculateFactorial(integer);
+    document.getElementById("placeholder").innerHTML = result;
 
-  let factoring = "";
-  for (let i = 1; i <= integer; i++) {
-    factoring += i !== integer ? i + " x " : i;
-  }
+    let factoring = "";
+    for (let i = 1; i <= integer; i++) {
+      factoring += i !== integer ? i + " x " : i;
+    }
 
-  document.getElementById("userInput").innerHTML = `${integer}! = `;
-  document.getElementById("mathematics").innerHTML = factoring;
+    document.getElementById("userInput").innerHTML = `${integer}! = `;
+    document.getElementById("mathematics").innerHTML = factoring;
+  } else
+    document.getElementById("placeholder").innerHTML = "INPUT is too large";
 };
